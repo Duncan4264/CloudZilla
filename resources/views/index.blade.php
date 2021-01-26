976797e90f66bbcfaeff8147fd9da795b9b44022
Cyrus Duncan <cyrusd4264@gmail.com>
2021-01-25 19:15:37 -0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 @extends('layouts.cloudmaster')
-=======
-@extends('pages.layouts.app')
->>>>>>> 3801b9847f0304e766343d8ae1de75d6e0cd3117
 @section('title', 'CloudZilla')
     @section('content')
 @endsection
