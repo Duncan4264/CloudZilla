c19e6677907db0f3d35158952c999967c1f616ab
Cyrus Duncan <cyrusd4264@gmail.com>
2021-03-12 09:53:05 -0700

    Added Logging

---
+++ routes/web.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+ // Goes to the register view
+Route::get('/register', function () {
+  return view('register');
+});
+// Goes to the login view
+Route::get('/login', function () {
+   return view('login');
+  });
+
+Route::get('/home', function () {
+        return view('index');
+    });
+Route::get('/timecard', function () {
+      return view('timecard');
+    });
+<<<<<<< HEAD
+=======
+
+>>>>>>> b82cda31a398f301423cd7996644b77c41e60778
+Route::post('/dologin', 'LoginController@index');
+
+Route::post('/register', function() {
+   return view('register'); 
+});
+
+
+// Routes to the Register controller after submitting register forum
+Route::post('/doregister', "RegisterController@onRegister");
+
+Route::post('/doclockout', 'TimecardController@Clockout');
+
+Route::post('/doclockin', 'TimecardController@Clockin');
+
+
+        
